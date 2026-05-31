640e2111c4caa630474418accfb12be3c6d9e57b
vvvgvvvgvv <ww15114163583@qq.com>
2026-05-31 22:53:38 +0800

    init cuda project

---
+++ include/cuda_utils.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <cuda_runtime.h>
+#include <cstdio>
+#include <cstdlib>
+
+#define CUDA_CHECK(call) do {                                      \
+    cudaError_t err = (call);                                      \
+    if (err != cudaSuccess) {                                      \
+        std::fprintf(stderr,                                       \
+                     "CUDA error at %s:%d: %s\n",                  \
+                     __FILE__,                                     \
+                     __LINE__,                                     \
+                     cudaGetErrorString(err));                     \
+        std::exit(EXIT_FAILURE);                                   \
+    }                                                             \
+} while (0)
+
+#define CUDA_KERNEL_CHECK() do {                                   \
+    CUDA_CHECK(cudaGetLastError());                                \
+    CUDA_CHECK(cudaDeviceSynchronize());                           \
+} while (0)
